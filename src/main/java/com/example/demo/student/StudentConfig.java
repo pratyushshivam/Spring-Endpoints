d16bd5fffe7d40f0b540a867c46a916b6cc6d191
pratyushshivam <pratyushshivam7@gmail.com>
2021-04-01 11:22:13 +0530

    Adding to Postgre

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,4 +1,26 @@
 package com.example.demo.student;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
 public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+         return args -> {
+             Student pratyush = new Student("Pratyush", "pratyushshivam7@gmail.com",
+                     LocalDate.of(2000, Month.DECEMBER,5),21);
+             Student robin = new Student("Robin", "robin@gmail.com",
+                     LocalDate.of(2003, Month.DECEMBER,2),18);
+             repository.saveAll(
+                     List.of(pratyush,robin) // save a list of students to database
+             );
+
+         };
+    }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,16 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Autowired
+
     public List<Student> getStudents(){
         //	We write the return type so here the returntype is list of strings so we use list
-        return List.of(new Student(1L,"Pratyush", "pratyushshivam7@gmail.com",
-                LocalDate.of(2000, Month.DECEMBER,5),21));
+        return studentRepository.findAll(); // returns a list to us
     }
 }
